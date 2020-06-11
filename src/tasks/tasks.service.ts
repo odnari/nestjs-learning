931a5af81095d0f1f164753523ca3fb32289ec8c
Nick <nick@odnari.com>
2020-06-11 16:37:35 +0300

    feat: get and delete task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
         return this.tasks
     }
 
+    getTaskById(id: string): Task {
+        return this.tasks.find(t => t.id === id)
+    }
+
     createTask(createTaskDto: CreateTaskDto): Task {
         const {title, description} = createTaskDto
 
@@ -24,5 +28,9 @@ export class TasksService {
         this.tasks.push(task)
 
         return task
+    }
+
+    deleteTask(id: string): void {
+        this.tasks = this.tasks.filter(t => t.id !== id)
     }
 }
