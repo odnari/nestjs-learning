bd0a3af30b79b0cc2da4354cc2a3e3bf75b24b24
Nick <nick@odnari.com>
2020-06-12 16:36:32 +0300

    feat: guard tasks module

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,6 +8,7 @@ import {
     Patch,
     Post,
     Query,
+    UseGuards,
     UsePipes,
     ValidationPipe
 } from '@nestjs/common';
@@ -17,8 +18,10 @@ import {CreateTaskDto} from "./dto/create-task.dto";
 import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
 import {TaskStatusValidationPipe} from "./pipes/task-status-validation.pipe";
 import {Task} from "./task.entity";
+import {AuthGuard} from "@nestjs/passport";
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private tasksService: TasksService) {
     }
@@ -40,7 +43,10 @@ export class TasksController {
     }
 
     @Patch('/:id/status')
-    updateTaskStatus(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Promise<Task> {
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus
+    ): Promise<Task> {
         return this.tasksService.updateTaskStatus(id, status)
     }
 
