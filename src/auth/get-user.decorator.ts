e0e6ee2f33971992b3c2bbe1d95de9aabeacba3a
Nick <nick@odnari.com>
2020-06-12 15:47:33 +0300

    feat: getUser decorator

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,10 @@
+import {createParamDecorator, ExecutionContext} from "@nestjs/common";
+import {User} from "./user.entity";
+
+/**
+ * Usage: myController(@GetUser() user: User) {}
+ */
+export const GetUser = createParamDecorator((data, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest()
+    return req.user
+})
